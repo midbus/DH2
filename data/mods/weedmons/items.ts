347058ec560c38b94b320bc10d5661db0f9ff495
midbus <ajgreenwood101@gmail.com>
2025-03-09 22:40:37 -0400

    Create items.ts

---
+++ data/mods/weedmons/items.ts
@@ -0,0 +1,426 @@
+export const Items: {[itemid: string]: ModdedItemData} = {
+	//vanilla items
+	   diancite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mawilite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	heavydutyboots: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	choicespecs: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	choicescarf: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lifeorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	eviolite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	flameorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	toxicorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	airballoon: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lightclay: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	terrainextender: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	damprock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	heatrock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	smoothrock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	icyrock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	redcard: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ejectpack: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ejectbutton: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	weaknesspolicy: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	blunderpolicy: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	boosterenergy: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	throatspray: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	clearamulet: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	custapberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	focussash: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	occaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	passhoberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	rindoberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	wacanberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	yacheberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	colburberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	payapaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	habanberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	roseliberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	kasibberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	cobaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	chartiberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	shucaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tangaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	chopleberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	kebiaberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	babiriberry: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	firegem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	watergem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	grassgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	electricgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	icegem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	psychicgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	darkgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fairygem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	dragongem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	flyinggem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fightinggem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ghostgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	rockgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	steelgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	groundgem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	buggem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	poisongem: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	abomasite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	absolite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	aerodactylite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	aggronite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	alakazite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	altarianite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ampharosite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	banettite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	beedrillite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	blastoisinite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	blazikenite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	blueorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	redorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	cameruptite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	charizardinitex: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	charizardinitey: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	galladite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	garchompite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	gardevoirite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	gengarite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	glalitite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	gyaradosite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	houndoominite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	kangaskhanite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	latiasite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	latiosite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lopunnite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lucarionite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	manectite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	medichamite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	metagrossite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mewtwonitex: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mewtwonitey: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	pidgeotite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	pinsirite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sablenite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	salamencite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sceptilite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	scizorite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sharpedonite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	slowbronite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	steelixite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	swampertite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tyranitarite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	venusaurite: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+
+    },
+		
+}
